835a11595fbadfdba0b5e166da320e2d06478942
sameer2021skipq <sameer.jehan.s@skipq.org>
2021-12-20 05:39:28 +0000

    File Changed

--- sameerjehan/sprint1/SameerRepo/resources/s3Function.py
+++ sameerjehan/sprint1/SameerRepo/resources/s3Function.py
@@ -10,46 +10,13 @@ import os
 
 class s3PutData:
     def __init__(self):
-        # self.session = boto3.Session( 
-        #     aws_access_key_id='AKIAUTEXLE6CGARZHOO6', 
-        #     aws_secret_access_key='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')
-            
-        # self.resource = self.session.resource('s3')
+       
         
         self.resource = boto3.resource('s3')
         self.db = boto3.resource('dynamodb')
         
         
         
-    def s3_data(self):
-        session = boto3.Session( 
-         aws_access_key_id='AKIAUTEXLE6CGARZHOO6', 
-         aws_secret_access_key='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')
-         
-        s3 = session.resource('s3')
-        
-        bucket = s3.Bucket('sameerbucketnew')
-
-        for obj in bucket.objects.all():
-            key = obj.key
-            body = obj.get()['Body'].read()
-            
-        my_json = body.decode('utf8').replace("'", '"')
-        changed_string = str(my_json)
-        url_dict = eval(changed_string)
-        l1 = []
-        for urls in list(url_dict.keys()):
-            l1.append(url_dict[urls])
-            
-      #  constants.URL_TO_MONITOR = l1[0]
-        
-        # constants.URL_TO_MONITOR_ONE = l1[0]
-        # constants.URL_TO_MONITOR_TWO = l1[1]
-        # constants.URL_TO_MONITOR_THREE = l1[2]
-        # constants.URL_TO_MONITOR_FOUR = l1[3]
-        
-            
-        return l1
         
     def downloadData(self):
         BUCKET_NAME = 'my-bucket' # replace with your bucket name
