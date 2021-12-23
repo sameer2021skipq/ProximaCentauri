65110d43570026af78c6d5c6bc07308fd64da41f
sameer2021skipq <sameer.jehan.s@skipq.org>
2021-12-23 17:58:51 +0000

    cdk.json updated

--- sameerjehan/sprint2/SameerRepo/app.py
+++ sameerjehan/sprint2/SameerRepo/app.py
@@ -14,7 +14,7 @@ from sameer_repo.pipeline_stack import PipelineStack
 
 
 app = core.App()
-SameerRepoStack(app, "SameerRepoStack",
+PipelineStack(app, "SameerSkipqQPipeline",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
